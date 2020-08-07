7c851706f9b363352c5d9f3f70dbc2a2715fea25
AchalShah1006 <shahachal.32@gmail.com>
2020-08-07 16:06:02 +0530

    Updates

---
+++ precache-manifest.7553592f321b7a0dadb7f08daeea853b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f8d431e7de1e6b702c14c056b0ce63",
+    "url": "/Portfolio-ReactJs/index.html"
+  },
+  {
+    "revision": "8e82831a7c6440a7db9c",
+    "url": "/Portfolio-ReactJs/static/js/2.e9cbc9b7.chunk.js"
+  },
+  {
+    "revision": "6734d0bfe367c40a5b2a23ab2f00b933",
+    "url": "/Portfolio-ReactJs/static/js/2.e9cbc9b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6415cd376783c5b78455",
+    "url": "/Portfolio-ReactJs/static/js/main.794f83c4.chunk.js"
+  },
+  {
+    "revision": "093441e9ed61a6bdc850",
+    "url": "/Portfolio-ReactJs/static/js/runtime-main.529dedfe.js"
+  },
+  {
+    "revision": "38a4d574c926561492f1a34276e796e6",
+    "url": "/Portfolio-ReactJs/static/media/Bookstore.38a4d574.png"
+  },
+  {
+    "revision": "18229fae185d609d98ca59c1252d56d2",
+    "url": "/Portfolio-ReactJs/static/media/Portfolio-2.18229fae.png"
+  },
+  {
+    "revision": "e1c65422897c880e6bf3e9ccd856c868",
+    "url": "/Portfolio-ReactJs/static/media/aio-bot.e1c65422.png"
+  },
+  {
+    "revision": "a3971ba7370869d793c8615aceea08fc",
+    "url": "/Portfolio-ReactJs/static/media/covid19.a3971ba7.png"
+  },
+  {
+    "revision": "fa46615e0f386edd1655f230f4f7a826",
+    "url": "/Portfolio-ReactJs/static/media/portfolio.fa46615e.png"
+  },
+  {
+    "revision": "165a1c65ea94aeb33be51575a54a9cb4",
+    "url": "/Portfolio-ReactJs/static/media/pp.165a1c65.jpg"
+  }
+]);
